2afb5ee07572441d535d158f4c51a722d96abf2a
Rubal Walia <rubal.walia@gmail.com>
2018-03-29 19:36:22 +0530

    Added bootstrapper.

---
+++ VsDownloader/Bootstrapper.cs
@@ -0,0 +1,38 @@
+﻿namespace VsDownloader
+{
+    sealed class Bootstrapper
+    {
+        static object _syncRoot;
+        static Bootstrapper _instance;
+
+        const string URL = "https://docs.microsoft.com/en-us/visualstudio/install/";
+
+        static Bootstrapper()
+        {
+            _syncRoot = new object();
+        }
+
+        private Bootstrapper()
+        {
+
+        }
+
+        #region properties
+
+        public static Bootstrapper Instance
+        {
+            get
+            {
+                lock(_syncRoot)
+                {
+                    if (_instance == null)
+                        _instance = new Bootstrapper();
+
+                    return _instance;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
